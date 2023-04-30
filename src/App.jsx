d61a6ba74ea5fe4c62445c0f3f97377e0fb8d2fa
Tsetan Dorje <tsetangrace@gmail.com>
2023-04-30 19:27:36 +0530

    lazy load and suspense removed

--- src/App.jsx
+++ src/App.jsx
@@ -5,158 +5,25 @@ import {
   Route,
   RouterProvider,
 } from "react-router-dom";
-import { lazy, Suspense } from "react";
-// import Layout from "./pages/Layout.jsx";
-const Layout = lazy(() => import("./pages/Layout"));
-// import Home from "./pages/Home/Home";
-const Home = lazy(() => import("./pages/Home/Home"));
-// import About from "./pages/About";
-const About = lazy(() => import("./pages/About"));
-// import Contact from "./pages/Contact";
-const Contact = lazy(() => import("./pages/Contact"));
-const PrivacyPolicy = lazy(() => import("./pages/PrivacyPolicy"));
-const TermsOfService = lazy(() => import("./pages/TermsOfService"));
-// import CancellationPolicy from "./pages/CancellationPolicy";
-const CancellationPolicy = lazy(() => import("./pages/CancellationPolicy"));
-const LadakhTours = lazy(() => import("./pages/LadakhTours/LadakhTours"));
-const LadakhTourDetails = lazy(() =>
-  import("./components/LadakhTourDetails/LadakhTourDetails")
-);
-
-const HimachalTours = lazy(() => import("./pages/HimachalTours/HimachalTours"));
-const HimachalTourDetails = lazy(() =>
-  import("./components/HimachalTourDetails/HimachalTourDetails")
-);
-// import HimachalTourDetails from "./components/HimachalTourDetails/HimachalTourDetails";
-
-// import SouthIndiaTours from "./pages/SouthIndiaTours/SouthIndiaTours";
-const SouthIndiaTours = lazy(() =>
-  import("./pages/SouthIndiaTours/SouthIndiaTours")
-);
-const SouthIndiaTourDetails = lazy(() =>
-  import("./components/SouthIndiaTourDetails/SouthIndiaTourDetails")
-);
+import Layout from "./pages/Layout.jsx";
+import Home from "./pages/Home/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import PrivacyPolicy from "./pages/PrivacyPolicy";
+import TermsOfService from "./pages/TermsOfService";
+import CancellationPolicy from "./pages/CancellationPolicy";
+import LadakhTours from "./pages/LadakhTours/LadakhTours";
+import LadakhTourDetails from "./components/LadakhTourDetails/LadakhTourDetails";
+import HimachalTours from "./pages/HimachalTours/HimachalTours";
+import HimachalTourDetails from "./components/HimachalTourDetails/HimachalTourDetails";
+import SouthIndiaTours from "./pages/SouthIndiaTours/SouthIndiaTours";
+import SouthIndiaTourDetails from "./components/SouthIndiaTourDetails/SouthIndiaTourDetails";
 
 import "./App.css";
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: (
-      <Suspense fallback={<div>Loading...</div>}>
-        <Layout />
-      </Suspense>
-    ),
-    children: [
-      {
-        path: "/",
-        element: (
-          <Suspense fallback={<div>Loading...</div>}>
-            <Home />
-          </Suspense>
-        ),
-      },
-      {
-        path: "about",
-        element: (
-          <Suspense fallback={<div>Loading...</div>}>
-            <About />
-          </Suspense>
-        ),
-      },
-      {
-        path: "contact",
-        element: (
-          <Suspense fallback={<div>Loading...</div>}>
-            <Contact />
-          </Suspense>
-        ),
-      },
-      {
-        path: "Privacy-Policy",
-        element: (
-          <Suspense fallback={<div>Loading...</div>}>
-            <PrivacyPolicy />
-          </Suspense>
-        ),
-      },
-      {
-        path: "Cancellation-Policy",
-        element: (
-          <Suspense fallback={<div>Loading...</div>}>
-            <CancellationPolicy />
-          </Suspense>
-        ),
-      },
-      {
-        path: "Terms-Of-Service",
-        element: (
-          <Suspense fallback={<div>Loading...</div>}>
-            <TermsOfService />
-          </Suspense>
-        ),
-      },
-      {
-        path: "Destination/Ladakh",
-        element: (
-          <Suspense fallback={<div>Loading...</div>}>
-            <LadakhTours />
-          </Suspense>
-        ),
-      },
-      {
-        path: "Ladakh/:slug",
-        element: (
-          <Suspense fallback={<div>Loading...</div>}>
-            <LadakhTourDetails />
-          </Suspense>
-        ),
-      },
-      {
-        path: "Destination/Himachal",
-        element: (
-          <Suspense fallback={<div>Loading...</div>}>
-            <HimachalTours />
-          </Suspense>
-        ),
-      },
-      {
-        path: "Himachal/:slug",
-        element: (
-          <Suspense fallback={<div>Loading...</div>}>
-            <HimachalTourDetails />
-          </Suspense>
-        ),
-      },
-      {
-        path: "Destination/South-India",
-        element: (
-          <Suspense fallback={<div>Loading...</div>}>
-            <SouthIndiaTours />
-          </Suspense>
-        ),
-      },
-      {
-        path: "South-India/:slug",
-        element: (
-          <Suspense fallback={<div>Loading...</div>}>
-            <SouthIndiaTourDetails />
-          </Suspense>
-        ),
-      },
-    ],
-  },
-]);
+
 function App() {
   return (
-    <RouterProvider router={router}>
-      <div className="app"></div>
-    </RouterProvider>
-  );
-}
-
-export default App;
-// {
-/* <div className="App">
+    <div className="App">
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Layout />}>
@@ -184,5 +51,8 @@ export default App;
           </Route>
         </Routes>
       </BrowserRouter>
-    </div> */
-// }
+    </div>
+  );
+}
+
+export default App;
