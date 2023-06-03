e708570a457be57f3c8782c4ef6f22a355a46112
Tsetan Dorje <tsetangrace@gmail.com>
2023-06-03 23:28:25 +0530

    h1 and h2 of hero bg color changed

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -7,13 +7,16 @@ function Hero() {
         <div className="relative isolate px-6 pt-14 lg:px-8">
           <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
             <div className="text-center">
-              <h2 className="px-4 mb-4">
-                <span className="text-[#3195ff] bg-gradient-to-l from-green-300 via-blue-500 to-purple-600 bg-clip-text font-semibold text-sm leading-6 text-transparent md:text-2xl">
+              <h2 className="text-2xl leading-6 text-center px-4 mb-4">
+                <span className="bg-clip-text text-transparent bg-gradient-to-l from-green-300 via-blue-500 to-purple-600 font-semibold">
                   Eco India Journeys
                 </span>
               </h2>
-              <h1 className="bg-gradient-to-l from-green-300 via-blue-500 to-purple-600 bg-clip-text font-bold text-transparent leading-1 tracking-tight text-2xl md:text-4xl">
-                Travel Agency, Ladakh, Kashmir, South India, Himachal and More..
+              <h1 className="text-4xl font-bold text-transparent md:text-6xl leading-1 tracking-tight sm:text-2xl">
+                <span className="bg-clip-text bg-gradient-to-l from-green-300 via-blue-500 to-purple-600">
+                  Travel Agency, &nbsp;Ladakh, &nbsp;Kashmir, &nbsp;South India,
+                  Himachal and More..
+                </span>
               </h1>
               <p className="mt-6 text-md leading-loose text-gray-700">
                 "Ready to explore India? Discover amazing destinations, indulge
